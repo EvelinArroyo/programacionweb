c04bf67cc24f626754831a44ead70a5417dd31f6
EvelinArroyo <l17111994@cdjuarez.tecnm.mx>
2020-10-31 14:24:02 -0600

    actualizacion 31 oct

---
+++ java/js/script.js
@@ -0,0 +1,14 @@
+console.log(5);
+console.log('Hola mundo!'.length);/* se puede usar comillas dobles tambien*/
+console.log(234.456);
+console.log(2+3);
+console.log(34-12);
+//tipos de datos: numericos,string, boolean,null,undefined,symbol,object
+//variables
+var x=10;  //version antigua
+x='max';
+let y=15; //version actual
+const vacio=false; //constante
+
+//slice
+console.log(x.slice(2,0));
